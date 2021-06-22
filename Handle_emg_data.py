0fd756be6f73f23f33391e158a7f4a93241d7dc3
Skudalen <markusskudal@gmail.com>
2021-06-22 21:00:51 +0200

    doc: comments

--- Handle_emg_data.py
+++ Handle_emg_data.py
@@ -8,17 +8,21 @@ class CSV_handler:
     def __init__(self):
         self.working_dir = str(Path.cwd())
 
+    # Makes dataframe from the csv files in the working directory
     def make_df(self, filename)
         filepath = self.working_dir + str(filename)
         df = pd.read_csv(filepath)
         return df
-    
+
+    # Extracts out the timestamp and the selected emg signal into a new dataframe 
     def get_time_emg_table(self, filename, emg_nr):
 
         tot_data_frame = make_df(self, filename)
         emg_str = "emg" + str(emg_nr)
-        filtered_df = tot_data_frame[emg_str]
+        filtered_df = tot_data_frame[["timestamp", emg_str]]
         return filtered_df
+
+    
 
 
 
